e4170173126f84776efa0740eaddecd8d73505e9
kholden4 <kholden4@lesley.edu>
2018-09-24 04:58:41 -0400

    create nav.php page and part9.php page

---
+++ nav.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Kristy's Portfolio & Blog | 18.09</title>
+        <link rel="stylesheet" type="text/css" href="format.css">
+        <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+
+        <nav>
+          <ul class="nav1">
+            <li><a href="index.php">HOMEPAGE</a></li>
+            <li><a href="barista.html">BARISTA</a></li>
+            <li><a href="photography.html">PHOTOGRAPHY</a></li>
+            <li><a href="metalart.html">METAL ARTS</a></li>
+            <li><a href="webdesign.html">WEB DESIGN</a></li>
+            <li><a href="uxdesign.html">UX DESIGN</a></li>
+            <li><a href="post.php">BLOG</a></li>
+            <li><a href="post-submission.php">CREATE NEW POST</a></li>
+          </ul>
+        </nav><!--close for navigation-->
+    </head><!--close for head-->
